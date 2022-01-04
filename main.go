e5e2cc8b67cf09f5dea405b9627ec1f5d66772fd
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-01-04 17:48:29 +0530

    Added Mutex from Sync Library for race condition issue

--- main.go
+++ main.go
@@ -13,38 +13,42 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
+	mex := &sync.Mutex{}
 	for i := 1; i < 7; i++ {
 		id := rnd.Intn(7)
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := getBookFromCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, mex *sync.Mutex) {
+			if b, ok := getBookFromCache(id, mex); ok {
 				fmt.Println("Fetched from cache")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := getBookFromDb(id); ok {
+		}(id, wg, mex)
+		go func(id int, wg *sync.WaitGroup, mex *sync.Mutex) {
+			if b, ok := getBookFromDb(id, mex); ok {
 				fmt.Println("Fetched from Database")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, mex)
 	}
 	wg.Wait()
 }
 
-func getBookFromCache(id int) (Book, bool) {
+func getBookFromCache(id int, mex *sync.Mutex) (Book, bool) {
+	mex.Lock()
 	b, ok := cache[id]
+	mex.Unlock()
 	return b, ok
 }
 
-func getBookFromDb(id int) (Book, bool) {
+func getBookFromDb(id int, mex *sync.Mutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range Books {
 		if id == b.ID {
-			// go run --race would still be present
+			mex.Lock()
 			cache[id] = b
+			mex.Unlock()
 			return b, true
 		}
 	}
