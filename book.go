cab01b7fa671fa8e749ae3dcfd315fadd83398f4
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-01-04 22:43:20 +0530

    Channel Example with Select Condtion

--- book.go
+++ book.go
@@ -11,9 +11,10 @@ type Book struct {
 
 func (b Book) String() string {
 	return fmt.Sprintf(
-		"Title:\t\t%q\n"+
+		"ID:\t\t%q\n"+
+			"Title:\t\t%q\n"+
 			"Author:\t\t%q\n"+
-			"Published:\t%v\n", b.Name, b.Author, b.Sold)
+			"Published:\t%v\n", b.ID, b.Name, b.Author, b.Sold)
 }
 
 var Books = []Book{
@@ -30,33 +31,69 @@ var Books = []Book{
 		Sold:   2000,
 	},
 	{
-		3,
-		"Power of Habit",
-		"Charles Duhigg",
-		3000,
+		ID:     3,
+		Name:   "Power of Habit",
+		Author: "Charles Duhigg",
+		Sold:   3000,
 	},
 	{
-		4,
-		"Why we Sleep",
-		"Mattews",
-		3000,
+		ID:     4,
+		Name:   "Why we Sleep",
+		Author: "Mattews",
+		Sold:   3000,
 	},
 	{
-		5,
-		"Make time",
-		"Mike",
-		600,
+		ID:     5,
+		Name:   "Make time",
+		Author: "Mike",
+		Sold:   600,
 	},
 	{
-		6,
-		"Money Matters",
-		"Shawn",
-		2617,
+		ID:     6,
+		Name:   "Money Matters",
+		Author: "Shawn",
+		Sold:   2617,
 	},
 	{
-		7,
-		"How to make friend and influence people",
-		"Dale Cargnie",
-		918,
+		ID:     7,
+		Name:   "How to make friend and influence people",
+		Author: "Dale Cargnie",
+		Sold:   918,
+	},
+	{
+		ID:     8,
+		Name:   "Atomic Habits",
+		Author: "James Clear",
+		Sold:   2000,
+	},
+	{
+		ID:     9,
+		Name:   "Power of Habit",
+		Author: "Charles Duhigg",
+		Sold:   3000,
+	},
+	{
+		ID:     10,
+		Name:   "Why we Sleep",
+		Author: "Mattews",
+		Sold:   3000,
+	},
+	{
+		ID:     11,
+		Name:   "Make time",
+		Author: "Mike",
+		Sold:   600,
+	},
+	{
+		ID:     12,
+		Name:   "Money Matters",
+		Author: "Shawn",
+		Sold:   2617,
+	},
+	{
+		ID:     13,
+		Name:   "How to make friend and influence people",
+		Author: "Dale Cargnie",
+		Sold:   918,
 	},
 }
--- main.go
+++ main.go
@@ -14,23 +14,40 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	mex := &sync.RWMutex{}
-	for i := 1; i < 7; i++ {
-		id := rnd.Intn(7)
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
+
+	for i := 1; i < 12; i++ {
+		id := rnd.Intn(12) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, mex *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, mex *sync.RWMutex, ch chan<- Book) {
 			if b, ok := getBookFromCache(id, mex); ok {
-				fmt.Println("Fetched from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, mex)
-		go func(id int, wg *sync.WaitGroup, mex *sync.RWMutex) {
-			if b, ok := getBookFromDb(id, mex); ok {
-				fmt.Println("Fetched from Database")
-				fmt.Println(b)
+		}(id, wg, mex, cacheCh)
+		go func(id int, wg *sync.WaitGroup, mex *sync.RWMutex, ch chan<- Book) {
+			if b, ok := getBookFromDb(id); ok {
+				mex.Lock()
+				cache[id] = b
+				mex.Unlock()
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, mex)
+		}(id, wg, mex, dbCh)
+
+		go func(dbCh, cacheCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("From cache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("From database")
+				fmt.Println(b)
+			}
+		}(dbCh, cacheCh)
+		time.Sleep(150 * time.Millisecond)
 	}
 	wg.Wait()
 }
@@ -42,13 +59,11 @@ func getBookFromCache(id int, mex *sync.RWMutex) (Book, bool) {
 	return b, ok
 }
 
-func getBookFromDb(id int, mex *sync.RWMutex) (Book, bool) {
+func getBookFromDb(id int) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range Books {
 		if id == b.ID {
-			mex.Lock()
-			cache[id] = b
-			mex.Unlock()
+
 			return b, true
 		}
 	}
